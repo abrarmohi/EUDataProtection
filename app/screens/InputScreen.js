ad807f0d56359717e595817bdbd1fbdb70cdc9c4
Zombiefied <jannik.timmer@hsrw.org>
2020-12-03 18:12:15 +0100

    feat: added some styling + flags to ResultScreen

--- app/screens/InputScreen.js
+++ app/screens/InputScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Button } from 'react-native';
-import { Picker } from '@react-native-picker/picker'
+import React from "react";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 class InputScreen extends React.Component {
   state = {
-    country: 'germany'
-  }
+    country: "germany",
+  };
 
   render() {
     return (
@@ -13,32 +13,39 @@ class InputScreen extends React.Component {
         <Picker
           selectedValue={this.state.country}
           style={styles.picker}
-          onValueChange={(value) =>
-            this.setState({country: value})
-        }>
+          onValueChange={(value) => this.setState({ country: value })}
+        >
           <Picker.Item label="Germany" value="germany" />
           <Picker.Item label="Denmark" value="denmark" />
         </Picker>
-        
-        <Button style={styles.button} title="Submit" onPress={() => this.props.navigation.navigate('Result', { country: this.state.country })} />
+
+        <Button
+          style={styles.button}
+          title="Submit"
+          onPress={() =>
+            this.props.navigation.navigate("Result", {
+              country: this.state.country,
+            })
+          }
+        />
       </SafeAreaView>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    picker: {
-      justifyContent: "flex-end",
-      width: 200,
-      height: 50,
-      marginBottom: 20
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  picker: {
+    justifyContent: "flex-end",
+    width: 200,
+    height: 50,
+    marginBottom: 20,
+  },
 });
 
-export default InputScreen
+export default InputScreen;
--- app/screens/ResultScreen.js
+++ app/screens/ResultScreen.js
@@ -1,37 +1,85 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import { Flag } from "react-native-svg-flagkit";
 
-const database = require('../database/database.json');
+const database = require("../database/database.json");
 
 class ResultScreen extends React.Component {
-    render() {
-        const country = database.country[this.props.route.params.country]
+  render() {
+    const country = database.country[this.props.route.params.country];
 
-        return (
-            <SafeAreaView style={styles.container}>
-                <Text>Name: {country.name}</Text>
-                <Text>Data Commissioner: {country.dataCommissioner}</Text>
-                <Text>National Supervisory Authority: {country.nationalSupervisoryAuthority}</Text>
-                <Text>Address: {country.address}</Text>
-                <Text>Telephone: {country.telephone}</Text>
-                <Text>E-Mail: {country.email}</Text>
-                <Text>Homepage: {country.homepage}</Text>
-                <Text>Specififc Law: {country.specificLaw}</Text>
-                <Text>Capital: {country.capital}</Text>
-                <Text>Population: {country.population}</Text>
-                <Text>Most Recent Law Cases: {country.mostRecentLawCases}</Text>
-            </SafeAreaView>
-        )
-    }
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container1}>
+          <View style={styles.container2}>
+            <Text numberOfLines={1} style={{ fontSize: 23 }}>
+              {country.name.toUpperCase()}
+            </Text>
+            <View style={styles.container3}>
+              <Text style={styles.textHeader}> Capital: </Text>
+              <Text style={styles.text}> {country.capital}</Text>
+            </View>
+            <View style={styles.container3}>
+              <Text style={styles.textHeader}> Population: </Text>
+              <Text style={styles.text}> {country.population}</Text>
+            </View>
+          </View>
+          <Flag id={country.code} />
+        </View>
+        <Text>Data Commissioner: {country.dataCommissioner}</Text>
+        <Text>
+          National Supervisory Authority: {country.nationalSupervisoryAuthority}
+        </Text>
+        <Text>Address: {country.address}</Text>
+        <Text>Telephone: {country.telephone}</Text>
+        <Text>E-Mail: {country.email}</Text>
+        <Text>Homepage: {country.homepage}</Text>
+        <Text>Specififc Law: {country.specificLaw}</Text>
+        <Text>Most Recent Law Cases: {country.mostRecentLawCases}</Text>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+  },
+  container1: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    flexWrap: "wrap",
+    padding: 27,
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+  },
+  container3: {
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  textHeader: {
+    backgroundColor: "#003399",
+    borderRadius: 10,
+    color: "#fff",
+    textAlign: "center",
+  },
+  text: {
+    color: "#003399",
+    textAlign: "center",
+  },
 });
 
-export default ResultScreen
+export default ResultScreen;
