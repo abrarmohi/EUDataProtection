94f809b2c114a18deaff64f0c1663c58769feadd
AahmedKhattab <ahmed-elsayed-elsayed.khattab@hsrw.org>
2020-12-06 20:23:24 +0100

    about us 2

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './app/screens/HomeScreen'
 import InputScreen from './app/screens/InputScreen';
 import ResultScreen from './app/screens/ResultScreen';
 import AboutUs from './app/screens/AboutUs';
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Input" component={InputScreen} />
         <Stack.Screen name="Result" component={ResultScreen} />
         <Stack.Screen name="AboutUs" component={AboutUs} />
