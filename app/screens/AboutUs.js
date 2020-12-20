c8f5ae7ee808ed4706c78b5c7944246b01eca420
Zombiefied <jannik.timmer@hsrw.org>
2020-12-20 03:31:07 +0100

    feat: style AboutUs page

--- app/screens/AboutUs.js
+++ app/screens/AboutUs.js
@@ -8,92 +8,168 @@ import {
   Text,
   View,
   Image,
+  ScrollView,
 } from "react-native";
 
 class AboutUs extends React.Component {
   render() {
     return (
-      //  const onPress = () => ;
-
       <SafeAreaView style={styles.container}>
-        <View style={styles.sides}>
-          <TouchableOpacity style={styles.title}>
-            <FontAwesome5 name={"arrow-left"} size={20} color="black" />
-          </TouchableOpacity>
-          <Text style={styles.titletext}> About us:</Text>
-        </View>
-        <View style={styles.top}>
-          <Text>Manager:</Text>
-          <Text>Prof. Dr. Dieter Kopetz</Text>
-          <Text>dieter.kopetz@hochschule-rhein-waal.de</Text>
-          <Text>Friedrich-Heinrich-Allee 25</Text>
-          <Text>47475, Kamp-Lintfort, Germany.</Text>
-          <Text>+49 2842 908 25 0</Text>
-        </View>
-        <View style={styles.middle}>
-          <Text>Developers:</Text>
-          <Text>Abdalrahman Elmessiry</Text>
-          <Text>Abrar-Mohi Shafee</Text>
-          <Text>Ahmed Khattab</Text>
-          <Text>Anas Abdulhalim</Text>
-          <Text>Jannik Timmer</Text>
-          <Text>Lakhan-Radhakrishna Vaishnav</Text>
-        </View>
-        <View style={styles.bottom}>
-          <Text>Project members:</Text>
-          <Text>Arzu Yaman</Text>
-          <Text>John-Pingpoh Chokeh</Text>
-          <Text>Maida Sinanovic</Text>
-          <Text>Marie-Luise Speer</Text>
-          <Text>Merve Cura</Text>
-          <Text>Ramazana Omerovic</Text>
-          <Text>Seyda Simsek</Text>
-        </View>
+        <ScrollView>
+          <View style={styles.container1}>
+            <View style={styles.container4}>
+              <View style={styles.container3}>
+                <Text style={styles.textHeader}> MANAGER </Text>
+                <Text style={styles.text}>Prof. Dr. Dieter Kopetz</Text>
+                <Text
+                  style={styles.textLink}
+                  onPress={() =>
+                    Linking.openURL(
+                      "mailto:" + "dieter.kopetz@hochschule-rhein-waal.de"
+                    )
+                  }
+                >
+                  dieter.kopetz@hochschule-rhein-waal.de
+                </Text>
+                <Text style={styles.text}>Friedrich-Heinrich-Allee 25</Text>
+                <Text style={styles.text}>47475, Kamp-Lintfort, Germany.</Text>
+                <Text
+                  style={styles.textLink}
+                  onPress={() => Linking.openURL("tel:" + "+49 2842 908 25 0")}
+                >
+                  +49 2842 908 25 0
+                </Text>
+              </View>
+
+              <View style={styles.container3}>
+                <Text style={styles.textHeader}> DEVELOPERS </Text>
+                <Text style={styles.text}>Abdalrahman Elmessiry</Text>
+                <Text style={styles.text}>Abrar-Mohi Shafee</Text>
+                <Text style={styles.text}>Ahmed Khattab</Text>
+                <Text style={styles.text}>Anas Abdulhalim</Text>
+                <Text style={styles.text}>Jannik Timmer</Text>
+                <Text style={styles.text}>Lakhan-Radhakrishna Vaishnav</Text>
+                <Text style={styles.text}>Arzu Yaman</Text>
+                <Text style={styles.text}>John-Pingpoh Chokeh</Text>
+                <Text style={styles.text}>Maida Sinanovic</Text>
+                <Text style={styles.text}>Marie-Luise Speer</Text>
+                <Text style={styles.text}>Merve Cura</Text>
+                <Text style={styles.text}>Ramazana Omerovic</Text>
+                <Text style={styles.text}>Seyda Simsek</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
 }
 
+const mainColor = "#000";
+const mainColor1 = "#003399";
+//const mainColor = "#fff";
+const tertColor = "#fff";
+//const tertColor = "#000";
+
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 30,
-    padding: 20,
     flex: 1,
-    backgroundColor: "lightblue",
+    backgroundColor: tertColor,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+  },
+  container1: {
+    backgroundColor: tertColor,
+    flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "stretch",
+    flexWrap: "wrap",
+    paddingTop: 27,
+    paddingLeft: 27,
+    paddingRight: 27,
   },
-  sides: {},
-  titletext: {
+  container2: {
+    backgroundColor: tertColor,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+  },
+  container4: {
+    backgroundColor: tertColor,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    width: "100%",
+  },
+  container3: {
+    backgroundColor: tertColor,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch",
+  },
+  flagBox: {
+    backgroundColor: mainColor,
+    padding: 2,
+    borderRadius: 3,
+    alignSelf: "flex-start",
+  },
+  textHeader2: {
+    borderRadius: 7,
+    fontSize: 16,
+    borderWidth: 1,
+    backgroundColor: tertColor,
+    borderColor: mainColor,
+    color: mainColor,
+    fontWeight: "bold",
+  },
+  textHeader: {
+    marginTop: 17,
+    borderRadius: 7,
+    fontSize: 16,
+    borderWidth: 1,
+    backgroundColor: tertColor,
+    borderColor: mainColor,
+    color: mainColor,
+    fontWeight: "bold",
+  },
+  textHeader1: {
+    marginTop: 17,
+    borderRadius: 7,
     fontSize: 20,
-    //    justifyContent: "center"
+    borderWidth: 1,
+    backgroundColor: tertColor,
+    borderColor: mainColor,
+    color: mainColor,
+    fontWeight: "bold",
   },
-  title: {
-    backgroundColor: "#FF0000",
-    padding: 10,
-    height: 40,
-    width: 40,
+  textShort: {
+    marginLeft: 3,
+    maxWidth: 117,
+    color: mainColor,
+    fontSize: 16,
   },
-  top: {
-    padding: 10,
-    flex: 0.25,
-    backgroundColor: "white",
-    borderWidth: 5,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+  text: {
+    marginLeft: 3,
+    color: mainColor,
+    fontSize: 16,
   },
-  middle: {
-    padding: 10,
-    flex: 0.27,
-    backgroundColor: "#ff39",
-    borderWidth: 5,
+  textLink: {
+    marginLeft: 3,
+    color: mainColor1,
+    fontSize: 16,
   },
-  bottom: {
-    padding: 10,
-    flex: 0.31,
-    backgroundColor: "#DB7093",
-    borderWidth: 5,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+  text1: {
+    marginLeft: 3,
+    color: mainColor,
+    fontSize: 20,
+  },
+  headline: {
+    color: mainColor,
+    width: 137,
+    fontSize: 22,
+    fontWeight: "bold",
   },
 });
+
 export default AboutUs;
